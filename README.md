20aa577804c5e3ad5976149d2ec4de565b60106a
pavittarsingh315 <pavittarsingh12364@gmail.com>
2022-02-15 17:42:15 -0800

    Second Commit

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 1. Ran: "go get -u github.com/gofiber/fiber/v2 go.mongodb.org/mongo-driver/mongo github.com/joho/godotenv github.com/go-playground/validator/v10"
 2. Set up project and database on mongoDB atlas.
 3. Got mongo connection uri from db atlas and created an env var in .env.
+
+## Second Commit
+
+1. Created a function to get the MongoURI from the env.
+2. Created a function to connect to the database.
+3. Created a function to get collections in the database.
---
+++ configs/setup.go
@@ -0,0 +1,40 @@
+package configs
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDb() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI(EnvMongoUri()))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, _ := context.WithTimeout(context.Background(), 10 * time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// ping the database
+    err = client.Ping(ctx, nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println("Connected to MongoDB")
+    return client
+}
+
+var DB *mongo.Client = ConnectDb()
+
+// getting database collections
+func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+    collection := client.Database("golangAPI").Collection(collectionName)
+    return collection
+}
